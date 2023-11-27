4fc59b74e355a8a81e6bc3331f1af8ba04c119a6
Crystalxy13 <146430660+Crystalxy13@users.noreply.github.com>
2023-11-27 21:05:51 +0800

    finish Task B

--- Template1Python.py
+++ Template1Python.py
@@ -52,3 +52,57 @@ print(num_accounts, identical_keys)
 # Paste output here
 6745 True
 """
+
+# ==============================================================================
+# Task B: Hash Creation and Comparison
+# ==============================================================================
+"""
+This section creates hash values for selected loan account data and compares these hash values.
+"""
+    
+# Function to create hash of a given account data
+def create_hash(account_data):
+    """
+    Creates a hash of the given account data.
+    :param account_data: The account data to be hashed.
+    :return: The hash value of the account data.
+    """
+    encoded_block = str(account_data).encode()
+    return _hashlib.sha256(encoded_block).hexdigest()
+
+# Create hash values for the 9th and 11th accounts
+hashvalue1 = create_hash(json_data[8])  # 9th account (index 8)
+hashvalue2 = create_hash(json_data[8])  # 9th account (index 8) again for comparison
+hashvalue3 = create_hash(json_data[10])  # 11th account (index 10)
+
+# Function to compare hash values
+def compare_hashes(hash1, hash2):
+    """
+    Compares two hash values and determines if they are identical or not.
+    :param hash1: The first hash value.
+    :param hash2: The second hash value.
+    :return: A string stating whether the hash values are identical or not.
+    """
+    if hash1 == hash2:
+        return "identical"
+    else:
+        return "not identical"
+
+# Compare hashvalue1 and hashvalue2, and hashvalue1 and hashvalue3
+comparison1 = compare_hashes(hashvalue1, hashvalue2)
+comparison2 = compare_hashes(hashvalue1, hashvalue3)
+
+print(f"Hash value of the 1st account (hashvalue1): {hashvalue1}")
+print(f"Hash value of the 1st account again (hashvalue2): {hashvalue2}")
+print(f"Hash value of the 2nd account (hashvalue3): {hashvalue3}")
+print(f"Comparison of hashvalue1 and hashvalue2 (Are they identical?): {comparison1}")
+print(f"Comparison of hashvalue1 and hashvalue3 (Are they identical?): {comparison2}")
+
+"""
+# Paste output here
+Hash value of the 1st account (hashvalue1): 0e40f0189fb34d58741ff6135db1e4cc97f46077e1f01c21d95b336334b43926
+Hash value of the 1st account again (hashvalue2): 0e40f0189fb34d58741ff6135db1e4cc97f46077e1f01c21d95b336334b43926
+Hash value of the 2nd account (hashvalue3): b311789a666d0e33bbd5704c84ace08cfd32cd611cbdfcfeb52cf9a6a62e9e85
+Comparison of hashvalue1 and hashvalue2 (Are they identical?): identical
+Comparison of hashvalue1 and hashvalue3 (Are they identical?): not identical
+"""
